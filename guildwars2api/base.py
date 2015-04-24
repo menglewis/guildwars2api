4943e4c0b8d761ac6c5de4c95fe8a063852c3245
James M. Allen <james.m.allen@gmail.com>
2015-04-24 15:54:17 -0400

    Adding functionality to support paging (via get_all and get_all_lazy) and passing multiple ids as sequences

--- guildwars2api/base.py
+++ guildwars2api/base.py
@@ -16,7 +16,8 @@ class BaseClient(object):
 
     def __init__(self, base_url=None, user_agent='Guild Wars 2 Python API Wrapper'):
         self.session = requests.Session()
-        self.session.headers.update({'User-Agent': user_agent, 'Accept': 'application/json'})
+        self.session.headers.update(
+            {'User-Agent': user_agent, 'Accept': 'application/json'})
         if base_url:
             self.host = base_url
         else:
@@ -27,6 +28,7 @@ class BaseClient(object):
 
 
 class BaseResource(object):
+
     """
     Base Class for a Guild Wars 2 Resource
     """
@@ -35,6 +37,49 @@ class BaseResource(object):
     def __init__(self, api_host, session):
         self.api_host = api_host
         self.session = session
+
+    def get_all_lazy(self, *args, **kwargs):
+        """
+        Gets all results for the resource as a Python object, using paging
+        (more info at https://wiki.guildwars2.com/wiki/API:2)
+        Doesn't fetch all results into a single object, but rather gets them
+        one batch at a time, suitable for use in a for-loop.
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        defaults = {
+            'page': 0,
+            'page_size': 200,
+        }
+        defaults.update(kwargs)
+        kwargs = defaults
+
+        page_total = kwargs['page'] + 1
+
+        while kwargs['page'] < page_total:
+            r = self.response(*args, **kwargs)
+            if r.status_code == 200:
+                for i in r.json():
+                    yield i
+
+                kwargs['page'] += 1
+                page_total = int(r.headers.get('X-Page-Total', 1))
+            else:
+                raise(GuildWars2APIError(r.reason))
+
+    def get_all(self, *args, **kwargs):
+        """
+        Gets all results for the resource as a Python object, using paging
+        (more info at https://wiki.guildwars2.com/wiki/API:2)
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        res = []
+        for r in self.get_all_lazy(*args, **kwargs):
+            res += r
+        return res
 
     def get(self, *args, **kwargs):
         """
@@ -57,6 +102,7 @@ class BaseResource(object):
         :return:
         """
         url = self.build_url(*args, **kwargs)
+        print url
         return self.session.get(url)
 
     def build_url(self, resource=None, *args, **kwargs):
@@ -70,5 +116,8 @@ class BaseResource(object):
         url = "%s/%s" % (self.api_host, resource)
         params = ""
         if len(kwargs) > 0:
+            for arg in kwargs:
+                if hasattr(kwargs[arg], '__iter__'):
+                    kwargs[arg] = ','.join(str(x) for x in kwargs[arg])
             params = "?{0}".format(urllib.urlencode(kwargs))
         return "{url}{params}".format(url=url, params=params)
